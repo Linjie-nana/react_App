32972c65094ac14d8e9e954b3bc9acd975adbcd8
linjiexin <770471223@qq.com>
2021-01-23 17:05:13 +0800

    获得城市数据修改，排序

--- src/components/city/city.jsx
+++ src/components/city/city.jsx
@@ -1,6 +1,68 @@
 import React, { Component } from 'react';
 import './city.css'
+import store from '../../store'
 class City extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            oCityList: [],
+            aCityKey: [],
+            oCurrentCity: store.getState()
+        }
+        this.unsubscrbe = store.subscribe(this.fnChangeCity)
+
+    }
+    //卸载前取消订阅
+    componentWillUnmount() {
+        this.unsubscrbe()
+    }
+    //订阅获取最新值得方法
+    fnChangeCity = () => {
+        this.setState({
+            oCurrentCity: store.getState()
+        })
+    }
+
+    componentDidMount() {
+        this.fnGetData()
+    }
+    fnGetData = async () => {
+        let Res = await this.axios.get('/area/city', {
+            params: {
+                level: 1
+            }
+        })
+        let { oCityList, aCityKey } = this.fnFormatData(Res.data.body);
+
+        //在原来数据的基础上添加 热门hot 城市
+        let Res2 = await this.axios.get('/area/hot');
+        oCityList.hot = Res2.data.body;
+        aCityKey.unshift('hot');
+
+        //在数据上添加 #当前城市
+        oCityList['#'] = [this.state.oCurrentCity]
+        aCityKey.unshift('#')
+
+        console.log(oCityList);
+        console.log(aCityKey);
+    }
+
+    fnFormatData(list) {
+        let oCityList = {}
+        list.forEach(item => {
+            let sLetter = item.short.substr(0, 1);
+            if (sLetter in oCityList) {
+                oCityList[sLetter].push(item)
+            } else {
+                oCityList[sLetter] = [item]
+            }
+        })
+        let aCityKey = Object.keys(oCityList).sort();
+        return {
+            aCityKey, oCityList
+        }
+    }
+
     render() {
         return (
             <div className={this.props.sClass}>
